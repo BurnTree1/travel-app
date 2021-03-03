f4b815239e2ccb6ead11c26ba9dad80e08b74f95
Ilya Yeliseyeu <Ilya.Yeliseyeu@ibm.com>
2021-03-03 22:39:24 +0300

    init: forming start configurations

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () =>
+    <h1 align="center"> Hello command</h1>
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
