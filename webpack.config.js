397f132e217f853de231d48861faa9b3572a3e73
Max Rebedailo <akrosom21@gmail.com>
2021-03-12 20:11:53 +0200

    fix: fix fetching countries

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    new ESLintPlugin()
+    new ESLintPlugin(),
   ],
   devServer: {
     overlay: true,
