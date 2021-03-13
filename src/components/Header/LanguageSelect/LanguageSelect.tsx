68f691d832d2c4e8494e80a28a2186f554f0c541
Max Rebedailo <akrosom21@gmail.com>
2021-03-13 23:46:33 +0200

    feat: add localstorage for language

--- src/components/Header/LanguageSelect/LanguageSelect.tsx
+++ src/components/Header/LanguageSelect/LanguageSelect.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import Select from '@material-ui/core/Select';
@@ -16,13 +16,24 @@ const mapDispatchToProps = { changeLanguage };
 
 interface IProps {
   language: { lang: string };
+
   changeLanguage(val: string): void;
 }
 
 const LanguageSelect: FC<IProps> = (props: IProps) => {
   const { language } = props;
   const [lang, setLang] = React.useState<string>(language.lang);
+  if (!localStorage.getItem('lang')) {
+    localStorage.setItem('lang', lang);
+  }
+  const localLang = localStorage.getItem('lang');
+  useEffect(() => {
+    if (localLang) {
+      props.changeLanguage(localLang);
+    }
+  }, []);
   const handleChange = (event: React.ChangeEvent<any>) => {
+    localStorage.setItem('lang', event.target.value);
     props.changeLanguage(event.target.value);
     setLang(event.target.value);
   };
@@ -31,17 +42,17 @@ const LanguageSelect: FC<IProps> = (props: IProps) => {
             <FormControl className={styles.formControl}>
                 <Select
                   id="select"
-                  value={lang}
+                  value={localLang}
                   onChange={handleChange}
                 >
                     <MenuItem value="en">
-                      <FormattedMessage id="language.en" />
+                        <FormattedMessage id="language.en" />
                     </MenuItem>
                     <MenuItem value="ru">
-                      <FormattedMessage id="language.ru" />
+                        <FormattedMessage id="language.ru" />
                     </MenuItem>
                     <MenuItem value="de">
-                      <FormattedMessage id="language.de" />
+                        <FormattedMessage id="language.de" />
                     </MenuItem>
                 </Select>
             </FormControl>
