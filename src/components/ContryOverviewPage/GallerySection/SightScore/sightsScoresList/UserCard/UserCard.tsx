2caaf60541d551a370db459f61af4a21c7bb611f
Kornienko Aleksey <kornienko199004@gmail.com>
2021-03-16 21:41:15 +0300

    fix: fix eslint errors

--- src/components/ContryOverviewPage/GallerySection/SightScore/sightsScoresList/UserCard/UserCard.tsx
+++ src/components/ContryOverviewPage/GallerySection/SightScore/sightsScoresList/UserCard/UserCard.tsx
@@ -3,7 +3,7 @@ import { Avatar, MenuItem } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { IScoreData } from 'Types';
-import { arrayBufferToBase64 } from 'Helpers/common.helpers';
+import { arrayBufferToBase64 } from '../../../../../../helpers/common.helpers';
 import styles from './UserCard.module.scss';
 import './userCard.css';
 
