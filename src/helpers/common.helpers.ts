79749a0562e19559535976c1693b9fbc6b52b584
Ilya Yeliseyeu <Ilya.Yeliseyeu@ibm.com>
2021-03-17 12:09:40 +0300

    fix: catch exception with image

--- src/helpers/common.helpers.ts
+++ src/helpers/common.helpers.ts
@@ -2,9 +2,14 @@ export const arrayBufferToBase64 = (
   img: { data: { type: string; data: number[] } },
 ): string | undefined => {
   if (img && img.data) {
-    // @ts-ignore
-    const base64String = btoa(String.fromCharCode(...new Uint8Array(img.data.data)));
-    const result = `data:image/jpeg;base64,${base64String}`;
+    let result = '';
+    try {
+      // @ts-ignore
+      const base64String = btoa(String.fromCharCode(...new Uint8Array(img.data.data)));
+      result = `data:image/jpeg;base64,${base64String}`;
+    } catch (e) {
+      console.log(e);
+    }
     return result;
   }
 
